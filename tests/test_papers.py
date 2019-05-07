ec46d1cf56ab954cfcad75d480d70470617dc6b4
kirk86 <kirk.86@aol.com>
2019-05-07 22:57:12 +0100

    Update test_papers.py

--- tests/test_papers.py
+++ tests/test_papers.py
@@ -94,10 +94,10 @@ class TestSimple(unittest.TestCase):
         self.assertTrue(os.path.exists(self.pdf))
 
     def test_doi(self):
-	val1 = run('papers doi '+self.pdf).strip()
-	val2 = self.doi
-	print("Types: {}, {}".format(type(val1), type(val2)))
-        self.assertEqual(run('papers doi '+self.pdf).strip(), self.doi)
+	#val1 = run('papers doi '+self.pdf).strip()
+	#val2 = self.doi
+	#print("Types: {}, {}".format(type(val1), type(val2)))
+        self.assertEqual(run('papers doi '+self.pdf).strip(""), self.doi)
 
     def test_fetch(self):
         bibtexs = run('papers fetch '+self.doi).strip()
