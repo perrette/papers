0140bca836d96659c0fdca7319b4cdc736628176
perrette <mahe.perrette@pik-potsdam.de>
2021-03-10 19:37:45 +0100

    decode() git command output before printing

--- papers/bib.py
+++ papers/bib.py
@@ -67,8 +67,8 @@ def append_abc(key, keys=[]):
 def generate_key(entry, nauthor=NAUTHOR, ntitle=NTITLE, minwordlen=3, mintitlen=4, keys=None):
     # names = bibtexparser.customization.getnames(entry.get('author','unknown').lower().split(' and '))
     names = family_names(entry.get('author','unknown').lower())
-    authortag = '_'.join([nm for nm in names[:nauthor]]) 
-    yeartag = entry.get('year','0000') 
+    authortag = '_'.join([nm for nm in names[:nauthor]])
+    yeartag = entry.get('year','0000')
     if not ntitle or not entry.get('title',''):
         titletag = ''
     else:
@@ -80,7 +80,7 @@ def generate_key(entry, nauthor=NAUTHOR, ntitle=NTITLE, minwordlen=3, mintitlen=
     if keys and key in keys: # and not isinstance(keys, set):
         key = append_abc(key, keys)
     return key
- 
+
 
 # DUPLICATE DEFINITION
 # ====================
@@ -140,7 +140,7 @@ def compare_entries(e1, e2, fuzzy=False):
 
     id1 = entry_id(e1)
     id2 = entry_id(e2)
-    
+
     logger.debug('{} ?= {}'.format(id1, id2))
 
     if id1 == id2:
@@ -171,7 +171,7 @@ def are_duplicates(e1, e2, similarity=DEFAULT_SIMILARITY, fuzzy_ratio=FUZZY_RATI
         PARTIAL = PARTIAL_DUPLICATES,
         FUZZY = FUZZY_DUPLICATES,
         )
-    try: 
+    try:
         target = level[similarity]
     except KeyError:
         raise ValueError('similarity must be one of EXACT, GOOD, FAIR, PARTIAL, FUZZY')
@@ -297,7 +297,7 @@ class Biblio(object):
 
         if i < len(self.entries) and self.key(self.entries[i]) == self.key(entry):
             logger.info('key duplicate: '+self.key(self.entries[i]))
-            
+
             if update_key:
                 newkey = self.append_abc_to_key(entry)  # add abc
                 logger.info('update key: {} => {}'.format(entry['ID'], newkey))
@@ -305,15 +305,15 @@ class Biblio(object):
 
             else:
                 raise DuplicateKeyError('this error can be avoided if update_key is True')
-       
-        else: 
+
+        else:
             logger.info('new entry: '+self.key(entry))
-        
+
         self.entries.insert(i, entry)
 
 
     def insert_entry_check(self, entry, update_key=False, mergefiles=True, on_conflict='i'):
-        
+
         duplicates = [e for e in self.entries if self.eq(e, entry)]
 
         if not duplicates:
@@ -383,7 +383,7 @@ class Biblio(object):
 
 
     def add_pdf(self, pdf, attachments=None, rename=False, copy=False, search_doi=True, search_fulltext=True, scholar=False, **kw):
-        
+
         bibtex = extract_pdf_metadata(pdf, search_doi, search_fulltext, scholar=scholar)
 
         bib = bibtexparser.loads(bibtex)
@@ -403,31 +403,31 @@ class Biblio(object):
 
         if rename:
             self.rename_entry_files(entry, copy=copy)
-            
+
 
     def scan_dir(self, direc, search_doi=True, search_fulltext=True, **kw):
-        
+
         for root, direcs, files in os.walk(direc):
             dirname = os.path.basename(root)
             if dirname.startswith('.'): continue
             if dirname.startswith('_'): continue
-        
+
             # maybe a special entry directory?
             if os.path.exists(hidden_bibtex(root)):
                 logger.debug('read from hidden bibtex')
                 try:
                     entry = read_entry_dir(root)
                     self.insert_entry(entry, **kw)
-                except Exception:  
+                except Exception:
                     logger.warn(root+'::'+str(error))
-                continue 
+                continue
 
             for file in files:
                 if file.startswith('.'):
                     continue
                 path = os.path.join(root, file)
                 try:
-                    if file.endswith('.pdf'): 
+                    if file.endswith('.pdf'):
                         self.add_pdf(path, search_doi=search_doi, search_fulltext=search_fulltext, **kw)
                     elif file.endswith('.bib'):
                         self.add_bibtex_file(path, **kw)
@@ -527,8 +527,8 @@ class Biblio(object):
                 continue
 
 
-    def fix_entry(self, e, fix_doi=True, fetch=False, fetch_all=False, 
-        fix_key=False, auto_key=False, key_ascii=False, encoding=None, 
+    def fix_entry(self, e, fix_doi=True, fetch=False, fetch_all=False,
+        fix_key=False, auto_key=False, key_ascii=False, encoding=None,
         format_name=True, interactive=False):
 
         e_old = e.copy()
@@ -592,10 +592,10 @@ class Biblio(object):
                 except Exception as error:
                     logger.warn('...failed to fetch/update bibtex (all): '+str(error))
 
-            if bibtex:                        
+            if bibtex:
                 db = bibtexparser.loads(bibtex)
                 e2 = db.entries[0]
-                self.fix_entry(e2, encoding=encoding, format_name=True) 
+                self.fix_entry(e2, encoding=encoding, format_name=True)
                 strip_e = lambda e_: {k:e_[k] for k in e_ if k not in ['ID', 'file'] and k in e2}
                 if strip_e(e) != strip_e(e2):
                     logger.info('...fetch-update entry')
@@ -658,12 +658,12 @@ def entry_filecheck_metadata(e, file, image=False):
         raise ValueError(e['ID']+': doi: entry <=> pdf : {} <=> {}'.format(e['doi'].lower(), doi.lower()))
 
 
-def entry_filecheck(e, delete_broken=False, fix_mendeley=False, 
+def entry_filecheck(e, delete_broken=False, fix_mendeley=False,
     check_hash=False, check_metadata=False, interactive=True, image=False):
 
     if 'file' not in e:
         return
-    
+
     if check_hash:
         import hashlib
 
@@ -679,8 +679,8 @@ def entry_filecheck(e, delete_broken=False, fix_mendeley=False,
             logger.info(e['ID']+': remove duplicate path: "{}"'.format(fixed.get(file, file)))
             continue
         realpaths.add(realpath) # put here so that for identical
-                                   # files that are checked and finally not 
-                                   # included, the work is done only once 
+                                   # files that are checked and finally not
+                                   # included, the work is done only once
 
         if fix_mendeley and not os.path.exists(file):
             old = file
@@ -695,8 +695,8 @@ def entry_filecheck(e, delete_broken=False, fix_mendeley=False,
             dirname = os.path.dirname(file)
             candidate = os.path.sep + file
             if (not file.startswith(os.path.sep) and dirname # only apply when some directory name is specified
-                and not os.path.exists(dirname) 
-                and os.path.exists(os.path.dirname(candidate))): # simply requires that '/'+directory exists 
+                and not os.path.exists(dirname)
+                and os.path.exists(os.path.dirname(candidate))): # simply requires that '/'+directory exists
                 # and os.path.exists(newfile)):
                     # logger.info('prepend "/" to file name: "{}"'.format(file))
                     file = candidate
@@ -766,16 +766,16 @@ def main():
 
     cfg = argparse.ArgumentParser(add_help=False, parents=[loggingp])
     grp = cfg.add_argument_group('config')
-    grp.add_argument('--filesdir', default=config.filesdir, 
+    grp.add_argument('--filesdir', default=config.filesdir,
         help='files directory (default: %(default)s)')
     grp.add_argument('--bibtex', default=config.bibtex,
         help='bibtex database (default: %(default)s)')
-    grp.add_argument('--dry-run', action='store_true', 
+    grp.add_argument('--dry-run', action='store_true',
         help='no PDF renaming/copying, no bibtex writing on disk (for testing)')
 
     # status
     # ======
-    statusp = subparsers.add_parser('status', 
+    statusp = subparsers.add_parser('status',
         description='view install status',
         parents=[cfg])
     statusp.add_argument('--no-check-files', action='store_true', help='faster, less info')
@@ -785,32 +785,32 @@ def main():
         if o.bibtex:
             config.bibtex = o.bibtex
         if o.filesdir is not None:
-            config.filesdir = o.filesdir        
+            config.filesdir = o.filesdir
         print(config.status(check_files=not o.no_check_files, verbose=o.verbose))
-        
+
 
     # install
     # =======
 
     installp = subparsers.add_parser('install', description='setup or update papers install',
         parents=[cfg])
-    installp.add_argument('--reset-paths', action='store_true') 
+    installp.add_argument('--reset-paths', action='store_true')
     # egrp = installp.add_mutually_exclusive_group()
-    installp.add_argument('--local', action='store_true', 
-        help="""save config file in current directory (global install by default). 
-        This file will be loaded instead of the global configuration file everytime 
-        papers is executed from this directory. This will affect the default bibtex file, 
+    installp.add_argument('--local', action='store_true',
+        help="""save config file in current directory (global install by default).
+        This file will be loaded instead of the global configuration file everytime
+        papers is executed from this directory. This will affect the default bibtex file,
         the files directory, as well as the git-tracking option. Note this option does
         not imply anything about the actual location of bibtex file and files directory.
         """)
-    installp.add_argument('--git', action='store_true', 
-        help="""Track bibtex files with git. 
+    installp.add_argument('--git', action='store_true',
+        help="""Track bibtex files with git.
         Each time the bibtex is modified, a copy of the file is saved in a git-tracked
-        global directory (see papers status), and committed. Note the original bibtex name is 
+        global directory (see papers status), and committed. Note the original bibtex name is
         kept, so that different files can be tracked simultaneously, as long as the names do
         not conflict. This option is mainly useful for backup purposes (local or remote).
         Use in combination with `papers git`'
-        """) 
+        """)
     installp.add_argument('--gitdir', default=config.gitdir, help='default: %(default)s')
 
     grp = installp.add_argument_group('status')
@@ -906,23 +906,23 @@ def main():
     # addp.add_argument('-f','--force', action='store_true', help='disable interactive')
 
     grp = addp.add_argument_group('duplicate check')
-    grp.add_argument('--no-check-duplicate', action='store_true', 
+    grp.add_argument('--no-check-duplicate', action='store_true',
         help='disable duplicate check (faster, create duplicates)')
-    grp.add_argument('--no-merge-files', action='store_true', 
+    grp.add_argument('--no-merge-files', action='store_true',
         help='distinct "file" field considered a conflict, all other things being equal')
-    grp.add_argument('-u', '--update-key', action='store_true', 
+    grp.add_argument('-u', '--update-key', action='store_true',
         help='update added key according to any existing duplicate (otherwise an error might be raised on identical insert key)')
     # grp.add_argument('-f', '--force', action='store_true', help='no interactive')
     grp.add_argument('-m', '--mode', default='i', choices=['u', 'U', 'o', 's', 'r', 'i','a'],
-        help='''if duplicates are found, the default is to start an (i)nteractive dialogue, 
+        help='''if duplicates are found, the default is to start an (i)nteractive dialogue,
         unless "mode" is set to (r)aise, (s)skip new, (u)pdate missing, (U)pdate with new, (o)verwrite completely.
         ''')
 
     grp = addp.add_argument_group('directory scan')
-    grp.add_argument('--recursive', action='store_true', 
+    grp.add_argument('--recursive', action='store_true',
         help='accept directory as argument, for recursive scan \
         of .pdf files (bibtex files are ignored in this mode')
-    grp.add_argument('--ignore-errors', action='store_true', 
+    grp.add_argument('--ignore-errors', action='store_true',
         help='ignore errors when adding multiple files')
 
     grp = addp.add_argument_group('pdf metadata')
@@ -932,9 +932,9 @@ def main():
 
     grp = addp.add_argument_group('attached files')
     grp.add_argument('-a','--attachment', nargs='+', help=argparse.SUPPRESS) #'supplementary material')
-    grp.add_argument('-r','--rename', action='store_true', 
+    grp.add_argument('-r','--rename', action='store_true',
         help='rename PDFs according to key')
-    grp.add_argument('-c','--copy', action='store_true', 
+    grp.add_argument('-c','--copy', action='store_true',
         help='copy file instead of moving them')
 
 
@@ -950,14 +950,14 @@ def main():
             logger.error('--attachment is only valid for one added file')
             addp.exit(1)
 
-        kw = {'on_conflict':o.mode, 'check_duplicate':not o.no_check_duplicate, 
+        kw = {'on_conflict':o.mode, 'check_duplicate':not o.no_check_duplicate,
             'mergefiles':not o.no_merge_files, 'update_key':o.update_key}
 
         for file in o.file:
             try:
                 if os.path.isdir(file):
                     if o.recursive:
-                        my.scan_dir(file, rename=o.rename, copy=o.copy, 
+                        my.scan_dir(file, rename=o.rename, copy=o.copy,
                             search_doi=not o.no_query_doi,
                             search_fulltext=not o.no_query_fulltext,
                               **kw)
@@ -965,22 +965,22 @@ def main():
                         raise ValueError(file+' is a directory, requires --recursive to explore')
 
                 elif file.endswith('.pdf'):
-                    my.add_pdf(file, attachments=o.attachment, rename=o.rename, copy=o.copy, 
+                    my.add_pdf(file, attachments=o.attachment, rename=o.rename, copy=o.copy,
                             search_doi=not o.no_query_doi,
-                            search_fulltext=not o.no_query_fulltext, 
-                            scholar=o.scholar, 
+                            search_fulltext=not o.no_query_fulltext,
+                            scholar=o.scholar,
                             **kw)
 
                 else: # file.endswith('.bib'):
                     my.add_bibtex_file(file, **kw)
 
             except Exception as error:
-                # print(error) 
+                # print(error)
                 # addp.error(str(error))
                 raise
                 logger.error(str(error))
                 if not o.ignore_errors:
-                    if len(o.file) or (os.isdir(file) and o.recursive)> 1: 
+                    if len(o.file) or (os.isdir(file) and o.recursive)> 1:
                         logger.error('use --ignore to add other files anyway')
                     addp.exit(1)
 
@@ -989,7 +989,7 @@ def main():
 
     # check
     # =====
-    checkp = subparsers.add_parser('check', description='check and fix entries', 
+    checkp = subparsers.add_parser('check', description='check and fix entries',
         parents=[cfg])
     checkp.add_argument('-k', '--keys', nargs='+', help='apply check on this key subset')
     checkp.add_argument('-f','--force', action='store_true', help='do not ask')
@@ -1029,8 +1029,8 @@ def main():
         for e in my.entries:
             if o.keys and e.get('ID','') not in o.keys:
                 continue
-            my.fix_entry(e, fix_doi=o.fix_doi, fetch=o.fetch, fetch_all=o.fetch_all, fix_key=o.fix_key, 
-                auto_key=o.auto_key, format_name=o.format_name, encoding=o.encoding, 
+            my.fix_entry(e, fix_doi=o.fix_doi, fetch=o.fetch, fetch_all=o.fetch_all, fix_key=o.fix_key,
+                auto_key=o.auto_key, format_name=o.format_name, encoding=o.encoding,
                 key_ascii=o.key_ascii, interactive=not o.force)
 
 
@@ -1044,34 +1044,34 @@ def main():
     # =====
     filecheckp = subparsers.add_parser('filecheck', description='check attached file(s)',
         parents=[cfg])
-    # filecheckp.add_argument('-f','--force', action='store_true', 
+    # filecheckp.add_argument('-f','--force', action='store_true',
     #     help='do not ask before performing actions')
 
     # action on files
-    filecheckp.add_argument('-r','--rename', action='store_true', 
+    filecheckp.add_argument('-r','--rename', action='store_true',
         help='rename files')
-    filecheckp.add_argument('-c','--copy', action='store_true', 
+    filecheckp.add_argument('-c','--copy', action='store_true',
         help='in combination with --rename, keep a copy of the file in its original location')
 
     # various metadata and duplicate checks
-    filecheckp.add_argument('--metadata-check', action='store_true', 
+    filecheckp.add_argument('--metadata-check', action='store_true',
         help='parse pdf metadata and check against metadata (currently doi only)')
 
-    filecheckp.add_argument('--hash-check', action='store_true', 
+    filecheckp.add_argument('--hash-check', action='store_true',
         help='check file hash sum to remove any duplicates')
 
-    filecheckp.add_argument('-d', '--delete-broken', action='store_true', 
+    filecheckp.add_argument('-d', '--delete-broken', action='store_true',
         help='remove file entry if the file link is broken')
 
-    filecheckp.add_argument('--fix-mendeley', action='store_true', 
+    filecheckp.add_argument('--fix-mendeley', action='store_true',
         help='fix a Mendeley bug where the leading "/" is omitted.')
 
-    filecheckp.add_argument('--force', action='store_true', help='no interactive prompt, strictly follow options') 
+    filecheckp.add_argument('--force', action='store_true', help='no interactive prompt, strictly follow options')
     # filecheckp.add_argument('--search-for-files', action='store_true',
     #     help='search for missing files')
     # filecheckp.add_argument('--searchdir', nargs='+',
     #     help='search missing file link for existing bibtex entries, based on doi')
-    # filecheckp.add_argument('-D', '--delete-free', action='store_true', 
+    # filecheckp.add_argument('-D', '--delete-free', action='store_true',
         # help='delete file which is not associated with any entry')
     # filecheckp.add_argument('-a', '--all', action='store_true', help='--hash and --meta')
 
@@ -1080,7 +1080,7 @@ def main():
 
         # fix ':home' entry as saved by Mendeley
         for e in my.entries:
-            entry_filecheck(e, delete_broken=o.delete_broken, fix_mendeley=o.fix_mendeley, 
+            entry_filecheck(e, delete_broken=o.delete_broken, fix_mendeley=o.fix_mendeley,
                 check_hash=o.hash_check, check_metadata=o.metadata_check, interactive=not o.force)
 
         if o.rename:
@@ -1265,12 +1265,12 @@ def main():
     doip = subparsers.add_parser('doi', description='parse DOI from PDF')
     doip.add_argument('pdf')
     doip.add_argument('--image', action='store_true', help='convert to image and use tesseract instead of pdftotext')
-    
+
     def doicmd(o):
         print(extract_pdf_doi(o.pdf, image=o.image))
 
     # fetch
-    # =====   
+    # =====
     fetchp = subparsers.add_parser('fetch', description='fetch bibtex from DOI')
     fetchp.add_argument('doi')
 
@@ -1307,7 +1307,7 @@ def main():
         shutil.move(tmp, back)
         savebib(None, o)
 
-        
+
 
     # git
     # ===
@@ -1318,7 +1318,7 @@ def main():
     def gitcmd(o):
         try:
             out = sp.check_output(['git']+o.gitargs, cwd=config.gitdir)
-            print(out)
+            print(out.decode())
         except:
             gitp.error('failed to execute git command')
 
