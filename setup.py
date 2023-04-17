f84f346a11add7d1651b4e3313aaedc9120d1e6e
perrette <mahe.perrette@pik-potsdam.de>
2023-04-17 11:31:51 +0200

    update to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,18 +1,5 @@
 from setuptools import setup
-import versioneer
-
-version = versioneer.get_version()
-
-setup(name='papers-cli',
-      version=version,
-      cmdclass = versioneer.get_cmdclass(),
-      author='Mahe Perrette',
-      author_email='mahe.perrette@gmail.com',
-      description='utilities to keep your PDF library organized',
-      url='https://github.com/perrette/papers',
-      download_url=f'https://github.com/perrette/papers/archive/{version}.tar.gz',
-      packages=['papers'],
-      scripts=['scripts/papers'],
-      license = "MIT",
-      requires=["bibtexparser", "crossrefapi", "rapidfuzz", "unidecode", "scholarly"],
-      )
+setup(
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    )
