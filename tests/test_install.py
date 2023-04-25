28710906fe853aa3c7f8afb84f8992598e126d8a
perrette <mahe.perrette@pik-potsdam.de>
2023-04-25 23:19:29 +0200

    some more (limited) install tests

--- tests/test_install.py
+++ tests/test_install.py
@@ -2,25 +2,83 @@ import os
 import shutil
 import tempfile
 import unittest
+import subprocess as sp
 # from pathlib import Path
 
-from tests.common import paperscmd
+from tests.common import paperscmd, prepare_paper, run
 
-class TestInstall(unittest.TestCase):
+bibtex = """@article{Perrette_2011,
+ author = {M. Perrette and A. Yool and G. D. Quartly and E. E. Popova},
+ doi = {10.5194/bg-8-515-2011},
+ journal = {Biogeosciences},
+ link = {https://doi.org/10.5194%2Fbg-8-515-2011},
+ month = {feb},
+ number = {2},
+ pages = {515--524},
+ publisher = {Copernicus {GmbH}},
+ title = {Near-ubiquity of ice-edge blooms in the Arctic},
+ volume = {8},
+ year = {2011}
+}"""
+
+class TestBaseInstall(unittest.TestCase):
 
     def setUp(self):
-        self.mybib = tempfile.mktemp(prefix='papers.bib')
-        self.filesdir = tempfile.mktemp(prefix='papers.files')
-
-    def test_local_install(self):
-        paperscmd(f'install --local --bibtex {self.mybib} --files {self.filesdir}')
-        self.assertTrue(os.path.exists(self.mybib))
-        self.assertTrue(os.path.exists(self.filesdir))
+        self.temp_dir = tempfile.TemporaryDirectory()
+        self.mybib = "papersxyz.bib"
+        self.filesdir = "filesxyz"
+        self.anotherbib = 'another.bib'
+        open(os.path.join(self.temp_dir.name, self.anotherbib), 'w').write(bibtex)
 
     def tearDown(self):
-        if os.path.exists(self.filesdir):
-            shutil.rmtree(self.filesdir)
-        if os.path.exists(self.mybib):
-            os.remove(self.mybib)
-        if os.path.exists('.papersconfig.json'):
-            os.remove('.papersconfig.json')
+        self.papers(f'uninstall')
+        self.temp_dir.cleanup()
+
+    # def _path(self, p):
+    #     return os.path.join(self.temp_dir, p)
+
+    def _exists(self, p):
+        return os.path.exists(os.path.join(self.temp_dir.name, p))
+
+    def papers(self, cmd, **kw):
+        return paperscmd(f'{cmd}', cwd=self.temp_dir.name, **kw)
+
+
+
+class TestLocalInstall(TestBaseInstall):
+
+    def test_install(self):
+        self.assertFalse(self._exists(self.mybib))
+        self.assertFalse(self._exists(self.filesdir))
+        self.papers(f'install --no-prompt --local --bibtex {self.mybib} --files {self.filesdir}')
+        self.assertTrue(self._exists(self.mybib))
+        self.assertTrue(self._exists(self.filesdir))
+
+
+class TestGlobalInstall(TestBaseInstall):
+
+    def test_install(self):
+        self.assertFalse(self._exists(self.mybib))
+        self.assertFalse(self._exists(self.filesdir))
+        self.papers(f'install --no-prompt --bibtex {self.mybib} --files {self.filesdir}')
+        self.assertTrue(self._exists(self.mybib))
+        self.assertTrue(self._exists(self.filesdir))
+
+
+class TestGitInstall(TestBaseInstall):
+
+    def test_install(self):
+        self.papers(f'install --local --no-prompt --bibtex {self.mybib} --files {self.filesdir} --git')
+        self.assertTrue(self._exists(self.mybib))
+        # self.assertTrue(self._exists(".git"))
+        # count = sp.check_output(f'cd {self.temp_dir.name} && git rev-list --all --count', shell=True).strip().decode()
+        # self.assertEqual(count, '0')
+        count = self.papers('git rev-list --all --count')
+        self.assertEqual(count, '0')
+
+        self.papers(f'add {self.anotherbib}')
+        # self.papers(f'add --doi 10.5194/bg-8-515-2011')
+        count = self.papers('git rev-list --all --count')
+        self.papers(f'status -v', sp_cmd='check_call')
+        self.assertEqual(count, '1')
+        self.papers(f'git log', sp_cmd='check_call')
