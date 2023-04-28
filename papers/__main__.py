a229d41b6a2e1c1e608e53d53f4b7e9e11689bdd
perrette <mahe.perrette@pik-potsdam.de>
2023-04-28 02:16:14 +0200

    add -1 shortcut for -l, --one-liner

--- papers/__main__.py
+++ papers/__main__.py
@@ -973,7 +973,7 @@ def get_parser(config=None):
 
     grp = listp.add_argument_group('formatting')
     mgrp = grp.add_mutually_exclusive_group()
-    mgrp.add_argument('-l', '--one-liner', action='store_true', help='one liner')
+    mgrp.add_argument('-l', '-1', '--one-liner', action='store_true', help='one liner')
     mgrp.add_argument('--key-only', action='store_true')
     mgrp.add_argument('-f', '--field', nargs='+', help='specific field(s) only')
     grp.add_argument('--no-key', action='store_true')
