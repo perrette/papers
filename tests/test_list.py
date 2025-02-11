549506610196613acf719b06abeddce22d914844
perrette <mahe.perrette@pik-potsdam.de>
2025-02-11 13:05:23 +0100

    updated test for one-linear printing by default

--- tests/test_list.py
+++ tests/test_list.py
@@ -40,99 +40,99 @@ class FormattingTest(ListTest):
 class SearchTest(ListTest):
 
     def test_list_title(self):
-        out = self.papers(f'list --title "ice-edge blooms"', sp_cmd='check_output')
+        out = self.papers(f'list --plain --title "ice-edge blooms"', sp_cmd='check_output')
         self.assertMultiLineEqual(out, self.initial_content)
 
     def test_list_title_fuzzy(self):
-        out = self.papers(f'list --title "ice edge bloom arctc"', sp_cmd='check_output')
+        out = self.papers(f'list --plain --title "ice edge bloom arctc"', sp_cmd='check_output')
         self.assertEqual(out, "")
 
-        out = self.papers(f'list --title "ice edge bloom arctc" --fuzzy', sp_cmd='check_output')
+        out = self.papers(f'list --plain --title "ice edge bloom arctc" --fuzzy', sp_cmd='check_output')
         self.assertMultiLineEqual(out, self.initial_content)
 
     def test_list_title_multiple(self):
-        out = self.papers(f'list --title ice edge', sp_cmd='check_output')
+        out = self.papers(f'list --plain --title ice edge', sp_cmd='check_output')
         self.assertMultiLineEqual(out, self.initial_content)
 
-        out = self.papers(f'list --title ice edge antarctic', sp_cmd='check_output')
+        out = self.papers(f'list --plain --title ice edge antarctic', sp_cmd='check_output')
         self.assertEqual(out, "")
 
-        out = self.papers(f'list --title ice edge antarctic --any', sp_cmd='check_output')
+        out = self.papers(f'list --plain --title ice edge antarctic --any', sp_cmd='check_output')
         self.assertMultiLineEqual(out, self.initial_content)
 
     def test_list_author(self):
-        out = self.papers(f'list --author perrette', sp_cmd='check_output')
+        out = self.papers(f'list --plain --author perrette', sp_cmd='check_output')
         self.assertMultiLineEqual(out, self.initial_content)
 
-        out = self.papers(f'list --author perrette balafon', sp_cmd='check_output')
+        out = self.papers(f'list --plain --author perrette balafon', sp_cmd='check_output')
         self.assertEqual(out, "")
 
-        out = self.papers(f'list --author perrette balafon --any', sp_cmd='check_output')
+        out = self.papers(f'list --plain --author perrette balafon --any', sp_cmd='check_output')
         self.assertMultiLineEqual(out, self.initial_content)
 
     def test_list_key(self):
-        out = self.papers(f'list --key perrette', sp_cmd='check_output')
+        out = self.papers(f'list --plain --key perrette', sp_cmd='check_output')
         self.assertMultiLineEqual(out, self.initial_content)
 
-        out = self.papers(f'list --key perrette --strict', sp_cmd='check_output')
+        out = self.papers(f'list --plain --key perrette --strict', sp_cmd='check_output')
         self.assertEqual(out, "")
 
-        out = self.papers(f'list --key perrette_2011 --strict', sp_cmd='check_output')
+        out = self.papers(f'list --plain --key perrette_2011 --strict', sp_cmd='check_output')
         self.assertMultiLineEqual(out, self.initial_content)
 
     def test_list_year(self):
-        out = self.papers(f'list --year 201', sp_cmd='check_output')
+        out = self.papers(f'list --plain --year 201', sp_cmd='check_output')
         self.assertMultiLineEqual(out, self.initial_content)
 
-        out = self.papers(f'list --year 201 --strict', sp_cmd='check_output')
+        out = self.papers(f'list --plain --year 201 --strict', sp_cmd='check_output')
         self.assertEqual(out, "")
 
-        out = self.papers(f'list --year 2011 --strict', sp_cmd='check_output')
+        out = self.papers(f'list --plain --year 2011 --strict', sp_cmd='check_output')
         self.assertMultiLineEqual(out, self.initial_content)
 
     def test_list_tag(self):
-        out = self.papers(f'list --tag kiwi', sp_cmd='check_output')
+        out = self.papers(f'list --plain --tag kiwi', sp_cmd='check_output')
         self.assertMultiLineEqual(out, self.initial_content)
 
-        out = self.papers(f'list --tag kiwi ocean', sp_cmd='check_output')
+        out = self.papers(f'list --plain --tag kiwi ocean', sp_cmd='check_output')
         self.assertMultiLineEqual(out, self.initial_content)
 
-        out = self.papers(f'list --tag kiwi bonobo', sp_cmd='check_output')
+        out = self.papers(f'list --plain --tag kiwi bonobo', sp_cmd='check_output')
         self.assertEqual(out, "")
 
-        out = self.papers(f'list --tag kiwi bonobo --any', sp_cmd='check_output')
+        out = self.papers(f'list --plain --tag kiwi bonobo --any', sp_cmd='check_output')
         self.assertMultiLineEqual(out, self.initial_content)
 
 
     def test_list_combined(self):
-        out = self.papers(f'list --year 2011 --author perrette', sp_cmd='check_output')
+        out = self.papers(f'list --plain --year 2011 --author perrette', sp_cmd='check_output')
         self.assertMultiLineEqual(out, self.initial_content)
 
         # Here we'd need the full name (never useful in practice)
-        out = self.papers(f'list --year 2011 --author perrette --strict', sp_cmd='check_output')
+        out = self.papers(f'list --plain --year 2011 --author perrette --strict', sp_cmd='check_output')
         self.assertEqual(out, "")
 
-        out = self.papers(f'list --year 2021 --author perrette', sp_cmd='check_output')
+        out = self.papers(f'list --plain --year 2021 --author perrette', sp_cmd='check_output')
         self.assertEqual(out, "")
 
         # --any has no effect on multiple strings
-        out = self.papers(f'list --year 2021 --author perrette --any', sp_cmd='check_output')
+        out = self.papers(f'list --plain --year 2021 --author perrette --any', sp_cmd='check_output')
         self.assertEqual(out, "")
 
 
     def test_list_fullsearch(self):
-        out = self.papers(f'list 2011 perrette', sp_cmd='check_output')
+        out = self.papers(f'list --plain 2011 perrette', sp_cmd='check_output')
         self.assertMultiLineEqual(out, self.initial_content)
 
         # --strict is deactivated
-        out = self.papers(f'list 2011 perrette --strict', sp_cmd='check_output')
+        out = self.papers(f'list --plain 2011 perrette --strict', sp_cmd='check_output')
         self.assertMultiLineEqual(out, self.initial_content)
 
-        out = self.papers(f'list 2021 perrette', sp_cmd='check_output')
+        out = self.papers(f'list --plain 2021 perrette', sp_cmd='check_output')
         self.assertEqual(out, "")
 
         # any works well on full search strings
-        out = self.papers(f'list 2021 perrette --any', sp_cmd='check_output')
+        out = self.papers(f'list --plain 2021 perrette --any', sp_cmd='check_output')
         self.assertMultiLineEqual(out, self.initial_content)
 
 
