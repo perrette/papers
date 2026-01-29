73cfe47b84ab77cc1754a81082f7da58b4fa6c04
Boyan Penkov <boyan.penkov@gmail.com>
2026-01-29 13:49:44 -0500

    OK, this runs in parallel and cleans up the race condition, and identifies a clear place for a fixture().  Also, runs this entire, relativly small file through black

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -8,20 +8,59 @@ from tests.common import paperscmd, prepare_paper
 
 class TestSimple(unittest.TestCase):
 
+    # TODO prepare_paper() here is clearly a ripe example for a fixture
     def setup_module(self):
-        self.pdf, self.doi, self.key, self.newkey, self.year, self.bibtex, self.file_rename = prepare_paper()
+        (
+            self.pdf,
+            self.doi,
+            self.key,
+            self.newkey,
+            self.year,
+            self.bibtex,
+            self.file_rename,
+        ) = prepare_paper()
         self.assertTrue(os.path.exists(self.pdf))
 
     def test_doi(self):
-        # TODO this relies on the prepare_paper() leaving the bg-8-515-2011.pdf file in dowloadedpapers
-        # and flakes out occasionally on py312 only
-        self.assertEqual(paperscmd(f'doi {self.pdf}', sp_cmd='check_output').strip(), self.doi)
+        (
+            self.pdf,
+            self.doi,
+            self.key,
+            self.newkey,
+            self.year,
+            self.bibtex,
+            self.file_rename,
+        ) = prepare_paper()
+        self.assertEqual(
+            paperscmd(f"doi {self.pdf}", sp_cmd="check_output").strip(), self.doi
+        )
 
     def test_fetch(self):
-        bibtexs = paperscmd(f'fetch {self.doi}', sp_cmd='check_output').strip()
+        (
+            self.pdf,
+            self.doi,
+            self.key,
+            self.newkey,
+            self.year,
+            self.bibtex,
+            self.file_rename,
+        ) = prepare_paper()
+        bibtexs = paperscmd(f"fetch {self.doi}", sp_cmd="check_output").strip()
         db1 = bibtexparser.loads(bibtexs)
         db2 = bibtexparser.loads(self.bibtex)
+        del bibtexs
         self.assertEqual(db1.entries, db2.entries)
+        del db1
+        del db2
 
     def test_fetch_scholar(self):
+        (
+            self.pdf,
+            self.doi,
+            self.key,
+            self.newkey,
+            self.year,
+            self.bibtex,
+            self.file_rename,
+        ) = prepare_paper()
         extract_pdf_metadata(self.pdf, scholar=True)
