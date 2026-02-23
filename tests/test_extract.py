79d0f89eafa432b2cbc39695dc81b30b2708b6d5
Boyan Penkov <boyan.penkov@gmail.com>
2026-02-23 17:08:04 -0500

    test works, but is not architecturally as brilliant as one might want

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -2,7 +2,7 @@ import unittest
 import os
 import tempfile
 import shutil
-
+import re
 from papers.extract import extract_pdf_metadata
 from papers.entries import parse_string
 
@@ -130,19 +130,37 @@ class TestRecursiveExtract(unittest.TestCase):
         self.assertTrue(os.path.exists(self.pdf1))
         self.assertTrue(os.path.exists(self.pdf2))
 
-    def test_doi(self):
-        self.assertEqual(
-            paperscmd(f"doi {self.pdf1}", sp_cmd="check_output").strip(), self.doi1
-        )
-
     def test_fetch(self):
-        bibtexs = paperscmd(f"extract {self.pdf1}", sp_cmd="check_output").strip()
-        db1 = parse_string(bibtexs)
-        db2 = parse_string(self.bibtex1)
-        self.assertEqual(
-            [dict(e.items()) for e in db1.entries],
-            [dict(e.items()) for e in db2.entries],
-        )
+        bibtexs = paperscmd(
+            f"extract --recursive {self.somedir}", sp_cmd="check_output"
+        ).strip()
+        the_right_answer = """@article{10.5194/bg-8-515-2011,
+        author = {Perrette, M. and Yool, A. and Quartly, G. D. and Popova, E. E.},
+        doi = {10.5194/bg-8-515-2011},
+        journal = {Biogeosciences},
+        number = {2},
+        pages = {515-524},
+        title = {Near-ubiquity of ice-edge blooms in the Arctic},
+        url = {https://doi.org/10.5194/bg-8-515-2011},
+        volume = {8},
+        year = {2011}
+        }
+        
+        @article{10.5194/esd-4-11-2013,
+        author = {Perrette, M. and Landerer, F. and Riva, R. and Frieler, K. and Meinshausen, M.},
+        doi = {10.5194/esd-4-11-2013},
+        journal = {Earth System Dynamics},
+        number = {1},
+        pages = {11-29},
+        title = {A scaling approach to project regional sea level rise and its uncertainties},
+        url = {https://doi.org/10.5194/esd-4-11-2013},
+        volume = {4},
+        year = {2013}
+        }
+        """
+        processed_bibtexs = re.sub(r"\s+", "", bibtexs)
+        processed_the_right_answer = re.sub(r"\s+", "", the_right_answer)
+        self.assertEqual(processed_bibtexs, processed_the_right_answer)
 
     def tearDown(self):
         os.remove(self.mybib)
